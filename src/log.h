2f682eacc2da20c4456223a57ec53ea9a2e44024
Ronsor <ronsor@protonmail.com>
2018-01-28 22:16:42 +0000

    Switch to sqlite3 backend

---
+++ src/log.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <time.h>
+#include <string.h>
+static inline char* _tstamp() {
+	static char myts[64];
+	time_t a;
+	time(&a);
+	strcpy(myts, ctime(&a));
+	myts[strlen(myts)-1] = 0;
+	return myts;
+}
+#define log_fatal(...) (fprintf(stderr, "FATAL [%s] ", _tstamp()), fprintf(stderr, __VA_ARGS__), abort(), 1)
+#define log_warn(...) (fprintf(stderr, "WARNING [%s] ", _tstamp()), fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n"))
+#define log_info(...) (fprintf(stderr, "INFO [%s] ", _tstamp()), fprintf(stderr, __VA_ARGS__), fprintf(stderr, "\n"))
